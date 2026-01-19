9ff9a34247713b76e52da357f1ef9a4941525c2d
raghad-coder <raghad.khaled666@gmail.com>
2026-01-20 00:01:22 +0200

    lastedit

--- src/data.js
+++ src/data.js
@@ -1,11 +1,13 @@
 export const areaData = [
-  { id: 1, top: '48.00%', left: '83.00%',  img: '/area1.png' },
-  { id: 2, top: '68.50%', left: '81.50%', img: '/area2.png' },
-  { id: 3, top: '83.20%', left: '65.40%', img: '/area3.png' },
-  { id: 4, top: '83.20%', left: '47.10%',img: '/area4.png' },
-  { id: 5, top: '48.30%', left: '49.20%', img: '/area5.png' },
-  { id: 6, top: '50.10%', left: '59.80%', img: '/area6.png' },
-  { id: 7, top: '28.50%', left: '68.20%', img: '/area7.png' },
-  { id: 8, top: '78.40%', left: '12.50%',img: '/area8.png' },
-  { id: 9, top: '36.20%', left: '20.10%', img: '/area9.png' },
+  { id: 1, top: '46.00%', left: '78.00%',  img: '/area1.png' },
+  { id: 2, top: '64.50%', left: '77.50%', img: '/area2.png' },
+  { id: 3, top: '75.20%', left: '63.40%', img: '/area3.png' },
+  { id: 4, top: '79.20%', left: '47.10%',img: '/area4.png' },
+  { id: 5, top: '47.30%', left: '49.20%', img: '/area5.png' },
+  { id: 6, top: '49.10%', left: '57.80%', img: '/area6.png' },
+  { id: 7, top: '28.50%', left: '65.20%', img: '/area7.png' },
+  { id: 8, top: '72.40%', left: '18.50%',img: '/area8.png' },
+  { id: 9, top: '36.20%', left: '23.10%', img: '/area9.png' },
+  { id: 10, top: '64.20%', left: '58.10%', img: '/area9.png' },
+
 ];
